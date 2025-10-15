efd54dd809663167f0db979c8370e4a087edee1b
Trthomaz <seuemail@example.com>
2025-10-15 01:18:12 -0300

    ajuste

--- backend/src/main/java/com/thiagothomaz/trabalho1/dto/aluno/AlunoRequest.java
+++ backend/src/main/java/com/thiagothomaz/trabalho1/dto/aluno/AlunoRequest.java
@@ -16,22 +16,3 @@ public class AlunoRequest {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 }
-package com.thiagothomaz.trabalho1.dto.aluno;
-
-public class AlunoRequest {
-    private String nome;
-    private String email;
-
-    public AlunoRequest() {}
-
-    public AlunoRequest(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
-
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-}
